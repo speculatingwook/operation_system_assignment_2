31d9082e7043371aa6be6360be251676ebc7e36c
Brian <bwook9908@gmail.com>
2024-06-06 22:05:30 +0900

    level1, 2 implemented

--- rwrlock.h
+++ rwrlock.h
@@ -1,5 +1,10 @@
+#include <semaphore.h>
 typedef struct _rwrlock_t {
+    sem_t lock;
+    sem_t writelock;
+    int readers;
 } rwrlock_t;
+
 void rwrlock_init(rwrlock_t *rwr);
 void rwrlock_acquire_writelock(rwrlock_t *rwr);
 void rwrlock_release_writelock(rwrlock_t *rwr);
@@ -7,12 +12,33 @@ void rwrlock_acquire_readlock(rwrlock_t *rwr);
 void rwrlock_release_readlock(rwrlock_t *rwr);
 
 void rwrlock_init(rwrlock_t *rwr){
+    sem_init(&rwr->lock, 0, 1);
+    sem_init(&rwr->writelock, 0, 1);
+    rwr->readers = 0;
 }
+
 void rwrlock_acquire_writelock(rwrlock_t *rwr){
+    sem_wait(&rwr->writelock);
 }
+
 void rwrlock_release_writelock(rwrlock_t *rwr){
+    sem_post(&rwr->writelock);
 }
+
 void rwrlock_acquire_readlock(rwrlock_t *rwr){
+    sem_wait(&rwr->lock);
+    rwr->readers++;
+    if (rwr->readers == 1){
+        sem_wait(&rwr->writelock);
+    }
+    sem_post(&rwr->lock);
 }
+
 void rwrlock_release_readlock(rwrlock_t *rwr){
+    sem_wait(&rwr->lock);
+    rwr->readers--;
+    if (rwr->readers == 0){
+        sem_post(&rwr->writelock);
+    }
+    sem_post(&rwr->lock);
 }
--- seqlock.h
+++ seqlock.h
@@ -1,5 +1,9 @@
+#include <semaphore.h>
 typedef struct _seqlock_t {
+    sem_t lock;
+    unsigned counter;
 } seqlock_t;
+
 void seqlock_init(seqlock_t *seq);
 void seqlock_write_lock(seqlock_t *seq);
 void seqlock_write_unlock(seqlock_t *seq);
@@ -7,17 +11,24 @@ unsigned seqlock_read_begin(seqlock_t *seq);
 unsigned seqlock_read_retry(seqlock_t *seq, unsigned cnt);
 
 void seqlock_init(seqlock_t *seq){
-
+    sem_init(&seq->lock, 0, 1);
+    seq->counter =0;
 }
+
 void seqlock_write_lock(seqlock_t *seq){
-
+    sem_wait(&seq->lock);
+    seq->counter++;
 }
+
 void seqlock_write_unlock(seqlock_t *seq){
-
+    seq->counter++;
+    sem_post(&seq->lock);
 }
+
 unsigned seqlock_read_begin(seqlock_t *seq){
-
+    return seq->counter;
 }
-unsigned seqlock_read_retry(seqlock_t *seq, unsigned cnt){
 
+unsigned seqlock_read_retry(seqlock_t *seq, unsigned cnt){
+    return seq->counter % 2 == 1 || cnt != seq->counter;
 }
